51b4c8cdd3e70c06a0638f19ecc34d59a57a3616
Cherv1213 <vlad70052@gmail.com>
2023-06-27 12:56:46 +0300

    main-page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 const serchIcon = document.querySelector('.header__search');
 const headerContainer = document.querySelector('.header__container');
 const serchForm = document.querySelector('.header-form');
+const serachInput = document.querySelector('#header-serch')
+const mainBlock = document.querySelector('main');
 
 serchIcon.addEventListener('click', () => {
    headerContainer.style.display = 'none';
    serchForm.style.display = 'flex';
+   mainBlock.style.paddingTop = '105px'
+   serachInput.focus();
 
-   // document.addEventListener('click',(el)=>{
-   //    const click = el.composedPath().includes(serchForm);
-   //    console.log(click);
-
-   //    if(!click){
-   //       headerContainer.style.display = 'block';
-   //       serchForm.style.display = 'none';
-   //    }
-   // })
+   mainBlock.addEventListener('click', () => {
+   headerContainer.style.display = 'block';
+   serchForm.style.display = 'none';
+   mainBlock.style.paddingTop = '0'
+   })
 })
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if(iconMenu){
+   iconMenu.addEventListener("click", function (e){
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
